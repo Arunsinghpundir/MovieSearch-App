43fc738f8775fae3a5efe84093f9a5f577f7d0c3
Arun Singh Pundir <Arunsinghpundir.325@gmail.com>
2022-12-21 02:01:15 +0530

    Add/Remove Button

--- src/component/MovieHeading.jsx
+++ src/component/MovieHeading.jsx
@@ -1,7 +1,7 @@
 export const MovieHeading = function(props){
     return (
         <div>
-            <h1>{props.heading}</h1>
+            <h1 className={props.class}>{props.heading}</h1>
         </div>
     )
 }
